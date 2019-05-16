25b7313dcddddf224f198b480adc0de79230f316
LaraShout <larashout@gmail.com>
2019-05-16 10:10:50 +0100

    admins model and migrations added

---
+++ database/seeds/AdminsTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Models\Admin;
+use Faker\Factory as Faker;
+use Illuminate\Database\Seeder;
+
+class AdminsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create();
+
+        Admin::create([
+            'name'      =>  $faker->name,
+            'email'     =>  'admin@admin.com',
+            'password'  =>  bcrypt('password'),
+        ]);
+    }
+}
