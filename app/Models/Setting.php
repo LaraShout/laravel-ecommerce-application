389d32fe4c33cee30129dc356071f7808cd6ef68
LaraShout <larashout@gmail.com>
2019-05-29 09:08:10 +0100

    set function was missing from settings model

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -29,4 +29,22 @@ class Setting extends Model
         }
         return $entry->value;
     }
+
+    /**
+     * @param $key
+     * @param null $value
+     * @return bool
+     */
+    public static function set($key, $value = null)
+    {
+        $setting = new self();
+        $entry = $setting->where('key', $key)->firstOrFail();
+        $entry->value = $value;
+        $entry->saveOrFail();
+        Config::set('key', $value);
+        if (Config::get($key) == $value) {
+            return true;
+        }
+        return false;
+    }
 }
